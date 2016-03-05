45e6e8551b3e81dbfda99160f6be3ef49df73000
Anthony Cho <s_eyeon@hotmail.com>
2016-03-05 12:55:10 -0700

    Anthony's pushing!

--- app/src/main/java/dagnss/com/parksfinder/MapsActivity.java
+++ app/src/main/java/dagnss/com/parksfinder/MapsActivity.java
@@ -34,13 +34,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+
+        /*
+        LatLng sydney = new LatLng(51.045, -114.057);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+
     }
+
 }
