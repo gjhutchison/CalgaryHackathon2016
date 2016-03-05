44a8510be3fcb7065942f01085d93bebae7cb15d
gjhutchison <grahamjhutchison@gmail.com>
2016-03-05 16:04:08 -0700

    AllEvents now threaded

--- app/src/main/java/dagnss/com/eventsDB/Event_Manager.java
+++ app/src/main/java/dagnss/com/eventsDB/Event_Manager.java
@@ -1,9 +1,12 @@
 package dagnss.com.eventsDB;
 
+import android.os.AsyncTask;
+
 import com.microsoft.windowsazure.mobileservices.*;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 /**
  * Created by Graham on 05/03/2016.
@@ -11,9 +14,15 @@ import java.net.MalformedURLException;
 public class Event_Manager {
     private MobileServiceClient mClient;
     private MobileServiceTable<events> mEventTable;
+
+    private ArrayList<events> eList;
+    private Boolean safe;
+
     public Event_Manager(MobileServiceClient client){
         mClient = client;
         mEventTable = mClient.getTable(events.class);
+
+        eList = new ArrayList<events>();
     }
 
     public MobileServiceList<events> getEventBySport(String sport){
@@ -31,27 +40,87 @@ public class Event_Manager {
     }
 
     /*
-    public MobileServiceList<events> getEventByDistance(float distance, float curLat, float curLong){
+    public MobileServiceList<events> getEventByID(String id){
         MobileServiceList<events> result = null;
 
         try{
-            result = mEventTable.where().field("")
-        }
 
-        return result;
+
+        }catch(Exception e){
+
+        }
     }
     */
-
-    public MobileServiceList<events> getAllEvents(){
+    public MobileServiceList<events> getEventsByDistance(float distance, double lat1, double lon1){
         MobileServiceList<events> result = null;
-        updateTable();
-        try {
+
+        ArrayList<String> ids = new ArrayList<String>();
+
+    /*
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = (sin(dlat/2))^2 + cos(lat1) * cos(lat2) * (sin(dlon/2))^2
+        c = 2 * atan2( sqrt(a), sqrt(1-a) )
+        d = R * c (where R is the radius of the Earth)
+     */
+
+        try{
             result = mEventTable.execute().get();
+
+            for(int i = 0;i<result.size();i++){
+                if(result.get(i)!=null){
+                    double lat2 = result.get(i).lati;
+                    double lon2 = result.get(i).longi;
+
+                    double dLon = lon2-lon1;
+                    double dLat = lat2-lat2;
+
+                    double a = Math.pow(Math.sin(dLat/2),2)+Math.cos(lat1)*Math.cos(lat2)*Math.pow(Math.sin(dLon/2),2);
+                    double c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+                    double d = 6373 * c;
+
+                    if(d > distance){
+                        ids.add(result.get(i).id);
+                    }
+                }
+            }
+
+            for(int i = 0;i<ids.size();i++){
+                for(int j = 0;j < result.size();j++){
+                    if(result.get(j).id.equals(ids.get(i))){
+                        result.remove(j);
+                        break;
+                    }
+                }
+            }
         }
-        catch(Exception e) {
+        catch(Exception e){
             e.printStackTrace();
         }
         return result;
+    }
+
+
+    public void getAllEvents(){
+        setSafe(false);
+        new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params){
+                MobileServiceList<events> result = null;
+                try{
+                    result = mEventTable.execute().get();
+                    eList.clear();
+                    for(events event : result){
+                        eList.add(event);
+                    }
+                    setSafe(true);
+                }
+                catch(Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
     }
 
     public void createEvent(String type, String desc, double eLat, double eLong){
@@ -71,4 +140,20 @@ public class Event_Manager {
         mEventTable = mClient.getTable(events.class);
     }
 
+    public void setSafe(Boolean b){
+        safe = b;
+    }
+
+    public Boolean listSafe(){
+        return safe;
+    }
+
+    public ArrayList<events> getEventList(){
+        if(listSafe()){
+            return eList;
+        }
+        else{
+            return null;
+        }
+    }
 }
