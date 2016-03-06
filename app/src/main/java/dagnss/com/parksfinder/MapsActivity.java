cc50adf3fdab00f5f18514af4213d21db9a91cad
Sven Zhang <zhangss@ucalgary.ca>
2016-03-06 12:12:51 -0700

    implemented a refresh button

--- app/src/main/java/dagnss/com/parksfinder/MapsActivity.java
+++ app/src/main/java/dagnss/com/parksfinder/MapsActivity.java
@@ -412,6 +412,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             case R.id.action_change_sport:
                 return true;
+            case R.id.action_refresh:
+                eventManager.updateTable();
+                eventManager.getAllEvents();
+                new LongOperation().execute("");
+
+                return true;
+
             default:
                 return super.onOptionsItemSelected( item );
         }
